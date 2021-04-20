e0d496fb35ba574e5efda7f2b28e5379e4a26a6b
Dimiter 'malkia' Stanev <malkia@gmail.com>
2021-04-19 17:36:31 -0700

    latest

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,108 @@
-import 'package:flutter/material.dart';
 import 'main_page.dart';
 
-void main() {
-  runApp(myApp());
-}
+import 'package:flutter/material.dart';
+
+//import 'package:bitsdojo_window/bitsdojo_window.dart';
 
 MaterialApp myApp() => MaterialApp(
       home: MainPage(),
       theme: ThemeData(fontFamily: "Nova Mono"),
     );
+
+void main() {
+  runApp(myApp());
+  // runApp(BitsDojoWidget());
+  // doWhenWindowReady(() {
+  //   final initialSize = Size(600, 450);
+  //   appWindow.minSize = initialSize;
+  //   appWindow.size = initialSize;
+  //   appWindow.alignment = Alignment.center;
+  //   appWindow.show();
+  // });
+}
+
+/*
+const borderColor = Color(0xFF805306);
+
+class BitsDojoWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: WindowBorder(
+                color: borderColor,
+                width: 1,
+                child: Row(children: [LeftSide(), RightSide()]))));
+  }
+}
+
+const sidebarColor = Color(0xFFF6A00C);
+
+class LeftSide extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: 200,
+        child: Container(
+            color: sidebarColor,
+            child: Column(
+              children: [
+                WindowTitleBarBox(child: MoveWindow()),
+                Expanded(child: Container())
+              ],
+            )));
+  }
+}
+
+const backgroundStartColor = Color(0xFFFFD500);
+const backgroundEndColor = Color(0xFFF6A00C);
+
+class RightSide extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [backgroundStartColor, backgroundEndColor],
+                  stops: [0.0, 1.0]),
+            ),
+            child: Column(children: [
+              WindowTitleBarBox(
+                  child: Row(children: [
+                Expanded(child: MoveWindow()),
+                WindowButtons()
+              ])),
+            ])));
+  }
+}
+
+final buttonColors = WindowButtonColors(
+    iconNormal: Color(0xFF805306),
+    mouseOver: Color(0xFFF6A00C),
+    mouseDown: Color(0xFF805306),
+    iconMouseOver: Color(0xFF805306),
+    iconMouseDown: Color(0xFFFFD500));
+
+final closeButtonColors = WindowButtonColors(
+    mouseOver: Color(0xFFD32F2F),
+    mouseDown: Color(0xFFB71C1C),
+    iconNormal: Color(0xFF805306),
+    iconMouseOver: Colors.white);
+
+class WindowButtons extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        MinimizeWindowButton(colors: buttonColors),
+        MaximizeWindowButton(colors: buttonColors),
+        CloseWindowButton(colors: closeButtonColors),
+      ],
+    );
+  }
+}
+*/
